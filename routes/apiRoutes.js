8a47498dba60b5130aa9de1597dbb8c732f5ec83
TJFitz <tjfitz@comcast.net>
2020-08-27 17:08:10 -0400

    get and post working, delete functionality next

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,19 @@
-const notes = require("../db/db.json");
+let notes = require("../db/db.json");
 const path = require("path");
+const fs = require("fs");
 
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     res.json(notes);
+  });
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    res.json(newNote);
   });
 };
