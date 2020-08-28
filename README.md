e4c17885716e3dace44b294bb189d777c88ed8c1
TJFitz <tjfitz@comcast.net>
2020-08-28 17:50:31 -0400

    final functionality finished

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Note_Taker
+
 My first full stack application using a server to keep notes
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,12 +8,21 @@ module.exports = (app) => {
   });
   app.post("/api/notes", (req, res) => {
     const newNote = req.body;
+    let noteId = Math.floor(Math.random() * 10000);
+    newNote.id = noteId;
     notes.push(newNote);
     fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
-      if (err) {
-        throw err;
-      }
+      if (err) throw err;
     });
     res.json(newNote);
+  });
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteNote = req.params.id;
+    const newNotes = notes.filter((note) => note.id !== Number(deleteNote));
+    fs.writeFile("db/db.json", JSON.stringify(newNotes), (err) => {
+      if (err) throw err;
+      res.json(newNotes);
+      location.reload();
+    });
   });
 };
